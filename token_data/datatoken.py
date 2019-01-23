863c2b095e20312185eb10e95e9ee77a223057b9
Seasa_Huang <ericet1234@gmail.com>
2019-01-24 04:38:09 +0800

    update token part

--- token_data/datatoken.py
+++ token_data/datatoken.py
@@ -16,7 +16,7 @@ ALPHANUMERIC_CHAR_SET = set(
 def count_up(file_name):
     data_temp = pd.read_csv(file_name)
 
-    ttype = ["source","target","language","year","acoustic","genre","context","o"]
+    ttype = ["target","language","year","acoustic","genre","context","o"]
     count = dict()
     for _ in ttype:
         count[_] = dict()
@@ -37,23 +37,24 @@ def count_up(file_name):
             except AttributeError:
                 continue
 
-    ttype = ["source","target","language","year","acoustic","genre","context","o"]
+    ttype = ["target","language","year","acoustic","genre","context","o"]
     for t in ttype:
         arr = [ (n,count[t][n]) for n in count[t]]
         arr = sorted(arr,key= lambda x:x[1],reverse=True)
         count[t] = arr
 
-    ttype = ["source","target","language","year","acoustic","genre","context","o"]
+    ttype = ["target","language","year","acoustic","genre","context","o"]
     token = dict()
     for t in ttype:
         token[t] = dict()
         with codecs.open('subword.{0}'.format(t),'w',"utf-8") as f_out:
             #for word token file add up some additional token
             #<PAD> <UNK> <SOS> <EOS>
-            if(t == 'source'):
-                f_out.write("'<pad>_'\n'<EOS>_'\n") 
-            elif(t == 'target'):
+            if(t == 'target'):
                 f_out.write("<pad>\n<EOS>\n<SOS>\n") 
+            elif(t == 'language'):
+                f_out.write("_Chinese\n_Taiwanese\n_Japanese\n_Cantonese\n_Korean\n_Western\n_None\n_Mix\n<english>\n<chinese>")
+                continue
             for i in range(len(count[t])):
                 f_out.write(u"{0}\n".format(count[t][i][0]))
 
@@ -67,25 +68,58 @@ def tokenlize(file_name):
         token[t] = dict()
         with codecs.open('subword.{0}'.format(t),'r','utf8') as f_in:
             for i,word in enumerate(f_in):
+                if(t == 'source'):
+                    word = word.strip()[1:-1]
+                    token[t][word] = str(len(token[t]))
+                 
+                else:
+                    word = word.strip()
+                    token[t][word] = str(len(token[t]))
 
-                word = word.strip()
-                token[t][word] = str(i)
-    
+    def convert(line):
+        out = []
+        start = 0
+        end = len(line)
+        while(start < end):
+            for mid in range(end,start,-1):
+                try:
+                    out.append(token['source'][line[start:mid]])
+                    start = mid
+                    break
+                except KeyError:
+                    pass
+        return out
+
     raw = pd.read_csv(file_name)
     def print_token(f_out,arr,ttype):
         if(isinstance(arr,float)):
             f_out.write(',"')
             f_out.write('"')
             return 
-            
-        arr = [ token[ttype][str(_)] for _ in arr.replace(',',' ').split()[:50]]
-        f_out.write(',"')
-        f_out.write(','.join(arr))
-        f_out.write('"')
+        if(ttype=='source'):
+            arr = arr.replace('_','\\u').strip()
+            out = []            
+            for word in arr.split():
+                data = [ _ in ALPHANUMERIC_CHAR_SET for _ in word ]
+                start = 0
+                for j in range(1,len(word)):
+                    if(data[j] != data[j-1]):
+                        out.extend(convert("{0}_".format(word[start:j])))
+                        start = j
+                else:
+                    out.extend(convert("{0}_".format(word[start:])))
+            f_out.write(',"')
+            f_out.write(','.join(out))
+            f_out.write('"')
+        else:
+            arr = [ token[ttype][str(_)] for _ in arr.replace(',',' ').split()[:50]]
+            f_out.write(',"')
+            f_out.write(','.join(arr))
+            f_out.write('"')
 
     ttype = ["source","target","language","year","acoustic","genre","context","o"]
-    with open("playlist_20181023_train.csv",'w') as f_train:
-        with open("playlist_20181023_valid.csv",'w') as f_valid:
+    with open("playlist_20181024_train.csv",'w') as f_train:
+        with open("playlist_20181024_valid.csv",'w') as f_valid:
             f_train.write('"id","source","target","language","year","acoustic","genre","context","o"\n')
             f_valid.write('"id","source","target","language","year","acoustic","genre","context","o"\n')
             for i in range(raw.shape[0]):
